f9b55563aad9d0ae0d2744942f8ea4e85eb31c1a
Jason Antonio Palacios <jason.palacios@utexas.edu>
2015-10-28 19:15:16 -0500

    Adding glm library for use in nodes
    .

--- node.h
+++ node.h
@@ -1,187 +1,190 @@
-// #ifndef __NODE_H__
-// #define __NODE_H__
+#ifndef __NODE_H__
+#define __NODE_H__
 
-// #include <GL/glut.h>
-// #include <vector>
-// #include "enums.h"
-// #include "geom.h"
+#include <GL/glut.h>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include <vector>
+#include "enums.h"
+#include "geom.h"
 
-// class Node
-// {
-// protected:
-//     std::vector<Node *> children_vec;
-// public:
-//     void addChild(Node *);
-//     virtual void traverse(void);
-// };
+class Node
+{
+protected:
+    std::vector<Node *> children_vec;
+public:
+    void addChild(Node *);
+    virtual void traverse(void);
+};
 
-// void Node::addChild(Node * child)
-// {
-//     children_vec.push_back(child);
-// }
+void Node::addChild(Node * child)
+{
+    children_vec.push_back(child);
+}
 
-// void Node::traverse(void)
-// {
-//     int children_vec_size = children_vec.size();
-//     for (int i = 0; i < children_vec_size; i++)
-//     {
-//         children_vec[i]->traverse();
-//     }
-// }
+void Node::traverse(void)
+{
+    int children_vec_size = children_vec.size();
+    for (int i = 0; i < children_vec_size; i++)
+    {
+        children_vec[i]->traverse();
+    }
+}
 
-// class ObjectNode : public Node
-// {
-// public:
-//     void traverse(void);
-// };
+class ObjectNode : public Node
+{
+public:
+    void traverse(void);
+};
 
-// void ObjectNode::traverse(void)
-// {
-//     glPushMatrix();
+void ObjectNode::traverse(void)
+{
+    glPushMatrix();
 
-//         Node::traverse();
+        Node::traverse();
 
-//     glPopMatrix();
-// }
+    glPopMatrix();
+}
 
-// class GeometryNode : public Node
-// {
-// private:
-//     Trimesh * mesh;
-// public:
-//     void traverse(void);
-// };
+class GeometryNode : public Node
+{
+private:
+    Trimesh * mesh;
+public:
+    void traverse(void);
+};
 
-// void GeomtryNode::traverse(void)
-// {
-//     mesh->render();
-// }
+void GeometryNode::traverse(void)
+{
+    mesh->render();
+}
 
-// class TransformNode : public Node
-// {
-// private:
-//     TRANS_ID type;
-//     float xyz[3];
-//     float xyz_inc[3];
-//     float angle;
-//     float angle_inc;
+class TransformNode : public Node
+{
+private:
+    TRANS_ID type;
+    float xyz[3];
+    float xyz_inc[3];
+    float angle;
+    float angle_inc;
 
-// public:
-//     void traverse(void);
-// };
+public:
+    void traverse(void);
+};
 
-// void TransformNode::traverse(void)
-// {
-//     switch(type)
-//     {
-//         case SCALE:
-//         {
-//             glScalef(it->xyz[0], it->xyz[1], it->xyz[2]);
-//             break;
-//         }
-//         case TRANSLATE:
-//         {
-//             glTranslatef(it->xyz[0], it->xyz[1], it->xyz[2]);
-//             break;
-//         }
-//         case ROTATE:
-//         {
-//             glRotatef(it->angle, it->xyz[0], it->xyz[1], it->xyz[2]);
-//             break;
-//         }
-//     }
+void TransformNode::traverse(void)
+{
+    switch(type)
+    {
+        case SCALE:
+        {
+            glScalef(xyz[0], xyz[1], xyz[2]);
+            break;
+        }
+        case TRANSLATE:
+        {
+            glTranslatef(xyz[0], xyz[1], xyz[2]);
+            break;
+        }
+        case ROTATE:
+        {
+            glRotatef(angle, xyz[0], xyz[1], xyz[2]);
+            break;
+        }
+    }
 
-//     Node::traverse();
-// }
+    Node::traverse();
+}
 
-// class AttributeNode : public Node
-// {
-// private:
-//     MODE_ID render_mode;
-// public:
-//     void traverse(void);
-// };
+class AttributeNode : public Node
+{
+private:
+    MODE_ID render_mode;
+public:
+    void traverse(void);
+};
 
-// void AttributeNode::traverse(void)
-// {
-//     switch(render_mode)
-//     {
-//         case MODE_POINTS:
-//         {
-//             // Point mode
-//             glDisable(GL_NORMALIZE);
-//             glDisable(GL_LIGHTING);
-//             glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
-//             break;
-//         }
-//         case MODE_WIREFRAME:
-//         {
-//             // Wireframe mode
-//             glDisable(GL_NORMALIZE);
-//             glDisable(GL_LIGHTING);
-//             glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-//             break;
-//         }
-//         case MODE_SOLID:
-//         {
-//             // Solid mode
-//             glDisable(GL_NORMALIZE);
-//             glDisable(GL_LIGHTING);
-//             glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-//             break;
-//         }
-//         case MODE_SHADED:
-//         {
-//             // Shaded mode
-//             glDisable(GL_NORMALIZE);
-//             glEnable(GL_LIGHTING);
-//             glEnable(GL_LIGHT0);
-//             glEnable(GL_COLOR_MATERIAL);
-//             glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-//             break;
-//         }
-//         case MODE_FACE_NORMS:
-//         {
-//             // Face normals mode
-//             glEnable(GL_NORMALIZE);
-//             glEnable(GL_LIGHTING);
-//             glEnable(GL_LIGHT0);
-//             glEnable(GL_COLOR_MATERIAL);
-//             glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-//             break;
-//         }
-//         case MODE_VERT_NORMS:
-//         {
-//             // Vertex normals mode
-//             glEnable(GL_NORMALIZE);
-//             glEnable(GL_LIGHTING);
-//             glEnable(GL_LIGHT0);
-//             glEnable(GL_COLOR_MATERIAL);
-//             glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-//             break;
-//         }
-//     }
+void AttributeNode::traverse(void)
+{
+    switch(render_mode)
+    {
+        case MODE_POINTS:
+        {
+            // Point mode
+            glDisable(GL_NORMALIZE);
+            glDisable(GL_LIGHTING);
+            glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+            break;
+        }
+        case MODE_WIREFRAME:
+        {
+            // Wireframe mode
+            glDisable(GL_NORMALIZE);
+            glDisable(GL_LIGHTING);
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+            break;
+        }
+        case MODE_SOLID:
+        {
+            // Solid mode
+            glDisable(GL_NORMALIZE);
+            glDisable(GL_LIGHTING);
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            break;
+        }
+        case MODE_SHADED:
+        {
+            // Shaded mode
+            glDisable(GL_NORMALIZE);
+            glEnable(GL_LIGHTING);
+            glEnable(GL_LIGHT0);
+            glEnable(GL_COLOR_MATERIAL);
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            break;
+        }
+        case MODE_FACE_NORMS:
+        {
+            // Face normals mode
+            glEnable(GL_NORMALIZE);
+            glEnable(GL_LIGHTING);
+            glEnable(GL_LIGHT0);
+            glEnable(GL_COLOR_MATERIAL);
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            break;
+        }
+        case MODE_VERT_NORMS:
+        {
+            // Vertex normals mode
+            glEnable(GL_NORMALIZE);
+            glEnable(GL_LIGHTING);
+            glEnable(GL_LIGHT0);
+            glEnable(GL_COLOR_MATERIAL);
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            break;
+        }
+    }
 
-//     Node::traverse();
-// }
+    Node::traverse();
+}
 
-// class LightNode : public Node
-// {
-// private:
-//     LIGHT_ID type;
-//     float pos[3];
-// public:
-//     void traverse(void);
-// };
+class LightNode : public Node
+{
+private:
+    LIGHT_ID type;
+    float pos[3];
+public:
+    void traverse(void);
+};
 
-// void LightNode::traverse(void)
-// {
+void LightNode::traverse(void)
+{
     
-// }
+}
 
-// class CameraNode : public Node
-// {
+class CameraNode : public Node
+{
 
-// };
+};
 
-// #endif
+#endif
