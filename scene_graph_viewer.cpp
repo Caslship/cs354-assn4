6de7b41ba21e9e42bdc9d320a2c8399be460f797
Jason Palacios <jason777@cs.utexas.edu>
2015-11-02 20:11:41 -0600

    Forgot to add a renamed file.

---
+++ scene_graph_viewer.cpp
@@ -0,0 +1,23 @@
+/*
+* Assignment #3
+* Name: Jason Palacios
+* UT EID: jap4839
+* UTCS: jason777
+*/
+
+#include <GL/glut.h>
+#include "helpers.h"
+
+int main(int argc, char ** argv)
+{  
+    // I like small main functions  
+    InitializeWindow(argc, argv);
+    InitializeGraphics();
+    RegisterCallbacks();
+    InitializeSceneGraph();
+    InitializeGUI();
+    ExecuteArguments(argc, argv);
+
+    glutMainLoop();
+    return 0;
+}
